1092881e71dc219dfa860764ee432f6263d5bb7d
aryanankit <a.arya8279@gmail.com>
2024-04-30 21:56:49 +0530

    Make some changes in chatbox components

--- sociopedia-ui/src/components/Messenger/ChatBoxComponent.js
+++ sociopedia-ui/src/components/Messenger/ChatBoxComponent.js
@@ -1,5 +1,6 @@
 import { Box } from "@mui/material";
 import React from "react";
+import './chatbox.css'
 
 const ChatBoxComponent = () => {
   return (<Box style={{ position: 'relative' , backgroundColor : 'blue', left : '0', bottom: '0', width: '10%', height: '100%' }}>
