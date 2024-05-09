cb409012b47c9b1930a2cea5c57acdebb5ea8f27
aryanankit <a.arya8279@gmail.com>
2024-05-10 00:02:40 +0530

    Make settings page

---
+++ sociopedia-ui/src/page/SettingsPage.js
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import React from "react";
+import './chatbox.css'
+
+const SettingPage = () => {
+  return (<Box 
+    style={{ position: 'relative' , backgroundColor : 'blue', left : '0', bottom: '0', width: '10%', height: '100%' }}
+  >
+    Setting Page !
+  </Box>)
+}
+
+export default SettingPage;
