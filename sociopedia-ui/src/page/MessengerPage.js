569b9f1c8ea9a6bce69be6e6a40310d2c6d4e3f4
aryanankit <a.arya8279@gmail.com>
2024-06-01 22:40:15 +0530

    Make changes to messenger pages

--- sociopedia-ui/src/page/MessengerPage.js
+++ sociopedia-ui/src/page/MessengerPage.js
@@ -1,14 +1,59 @@
-import { useEffect } from "react";
+import React, { useState } from 'react';
+import { Container, TextField, Button, Box, Typography, List, ListItem, ListItemText, Paper, Grid } from '@mui/material';
 
-const MessengerPage = () => {
+const Messenger = () => {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    document.title = 'Messenger';
-  }, []);
+  const handleSendMessage = () => {
+    if (message.trim()) {
+      setMessages([...messages, { text: message, sender: 'You' }]);
+      setMessage('');
+    }
+  };
 
-  return (<div>
-    Messenger Page
-  </div>)
-}
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSendMessage();
+    }
+  };
 
-export default MessengerPage;
+  return (
+    <Container maxWidth="md" sx={{ mt: 4 }}>
+      <Typography variant="h4" gutterBottom>
+        Messenger
+      </Typography>
+      <Paper sx={{ p: 2, mb: 2 }}>
+        <List sx={{ maxHeight: '60vh', overflow: 'auto' }}>
+          {messages.map((msg, index) => (
+            <ListItem key={index}>
+              <Grid container>
+                <Grid item xs={12}>
+                  <ListItemText primary={msg.sender} />
+                </Grid>
+                <Grid item xs={12}>
+                  <ListItemText primary={msg.text} />
+                </Grid>
+              </Grid>
+            </ListItem>
+          ))}
+        </List>
+      </Paper>
+      <Box sx={{ display: 'flex', gap: 1 }}>
+        <TextField
+          fullWidth
+          variant="outlined"
+          label="Type your message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyPress={handleKeyPress}
+        />
+        <Button variant="contained" color="primary" onClick={handleSendMessage}>
+          Send
+        </Button>
+      </Box>
+    </Container>
+  );
+};
+
+export default Messenger;
