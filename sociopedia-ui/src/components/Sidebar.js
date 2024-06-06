1d8a1326eef71758527725e4fbd44bac92355f6e
aryanankit <a.arya8279@gmail.com>
2024-06-06 18:34:36 +0530

    Make some changes to side bar

--- sociopedia-ui/src/components/Sidebar.js
+++ sociopedia-ui/src/components/Sidebar.js
@@ -1,10 +1,76 @@
-import { Box } from "@mui/material";
-import React from "react";
+import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 
-const Sidebar = () => {
-  return (<Box style={{ position: 'relative' , backgroundColor : 'blue', left : '0', bottom: '0', width: '10%', height: '100%' }}>
-    Sidebar
-  </Box>)
+const drawerWidth = 240;
+
+const StyledDrawer = styled(Drawer)({
+  width: drawerWidth,
+  flexShrink: 0,
+  '& .MuiDrawer-paper': {
+    width: drawerWidth,
+    boxSizing: 'border-box',
+  },
+});
+
+const StyledList = styled(List)({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+});
+
+function Sidebar({ children }) {
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const navigationItems = [
+    // Add your navigation items here
+    { text: 'Home', icon: <MenuIcon /> },
+    { text: 'About Us', icon: <MenuIcon /> },
+    { text: 'Contact', icon: <MenuIcon /> },
+  ];
+
+  return (
+    <>
+      <IconButton onClick={handleDrawerOpen} sx={{ color: 'white' }}>
+        <MenuIcon />
+      </IconButton>
+      <StyledDrawer
+        anchor="left"
+        open={open}
+        onClose={handleDrawerClose}
+      >
+        <IconButton onClick={handleDrawerClose}>
+          <ChevronLeftIcon />
+        </IconButton>
+        <StyledList>
+          {navigationItems.map((item) => (
+            <ListItem button key={item.text}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
+        </StyledList>
+      </StyledDrawer>
+      {children}
+    </>
+  );
 }
 
 export default Sidebar;
